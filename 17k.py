eb67c9fe5bb23696cb54129dd998f03918406907
Jarvis.Wu <shmily320700@gmail.com>
2014-10-10 14:20:23 +0800

    加注释和ReadMe说明

--- 17k.py
+++ 17k.py
@@ -10,17 +10,19 @@ opener.addheaders = [headers]
 ret = opener.open(qburl) 
 unipage = ret.read().decode("utf-8")
 
+#先截取整个列表内容部分中所有的<li>节点，每个节点又刚好榜单数据，再循环每个<li>即可
 contents = re.findall('<li.*?class="table_td">(.*?)</li>',unipage, re.S)
-for content in contents:
-	n = re.findall('<div.*?class="n">(.*?)</div>', content)[0]
 
+for content in contents:
+        #n 榜单排序序号
+	n = re.findall('<div.*?class="n">(.*?)</div>', content)[0]
+        #lb 类别
 	lb = re.findall('<div.*?class="lb">(.*?)</div>', content)[0]
 	lb = re.findall('<a.*?href=".*?".*?>(.*?)</a>', lb)[0]
-
-
+        #zp 作品
 	zp = re.findall('<div.*?class="zp">(.*?)</div>', content)[0]
 	zp = re.findall('<a.*?target="_blank".*?title=".*?".*?href=".*?">(.*?)</a>', zp)[0]
-
+        #zz 作者
 	zz = re.findall('<div.*?class="zz">(.*?)</div>', content)[0]
 	zz = re.findall('<a.*?target="_blank".*?href=".*?">(.*?)</a>', zz)[0]
 	print n,lb,zp,zz
--- baisi.py
+++ baisi.py
@@ -1,28 +1,33 @@
 #encoding:utf-8
-#爬虫百思不得姐
+#爬虫百思不得姐的段子
 import re
 import urllib2
 
+#格式化文本
 def format(content):
         content = re.sub(r'\n', '', content)
         content = re.sub(r'(<p>|</p>)', '', content)
         content = re.sub(r'(<br>|<br/>)', '', content)
         return content
 
+#获取指定页面的html
 def getPage(pg):
         qburl = "http://www.budejie.com/duanzi/" + str(pg)
+        #print '爬第' + str(pg) + '页'
         headers = ('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36')
         opener = urllib2.build_opener()
         opener.addheaders = [headers]
         ret = opener.open(qburl)
         page = ret.read().decode("utf-8")
         return page
-                        
+
+#通过正则将html页面的段子匹配出来       
 def getContents(pg):
         page = getPage(pg)
         contents = re.findall('<p class="web_size" id=".*?" style="WORD-BREAK: break-all;">(.*?)</div>', page, re.S)
         return contents
 
+#循环输出格式化之后的每个段子，输完读取下一页
 def printBs(pg):
         contents = getContents(pg)
         for content in contents:
